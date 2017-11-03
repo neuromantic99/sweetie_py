a11c109fdce4ffb115242cc0e1423192bd93a2aa
Unknown <james.rowland@ccc.ox.ac.uk>
2017-11-03 19:10:45 +0000

    2P behaviour added
    Updated the python workflow so it can read txt files from 2P behaviour

--- Python/Behaviour/workflow_behav.py
+++ Python/Behaviour/workflow_behav.py
@@ -1,5 +1,4 @@
 import readPyControl as pc
-import numpy as np
 import scipy.io as sio
 import os
 import errno
@@ -14,7 +13,7 @@ to a mouse and an individual matlab structure for each behavioural session
 
 '''
 
-fPath = '/home/jamesrowland/Documents/RawBehaviour/'
+fPath = '/home/jamesrowland/Documents/RawBehaviour/2p_behaviour/2017-10-18-hf_2p_whiskerstim_nowater'
 outPath = '/home/jamesrowland/Documents/ProcessedData/behaviour/'
 
 
@@ -22,28 +21,44 @@ outPath = '/home/jamesrowland/Documents/ProcessedData/behaviour/'
 def runWorkflow(txtFile, outPath):
 
     #call Tom's behaviour module    
+    
     my_session = pc.Session(txtFile, False)
+  
 
     # get the meta data from the header of the txt file
-    [ID, date, sessionType,go_pos] = getMetaData(my_session)
+    [ID, date, sessionType, go_pos] = getMetaData(my_session)
         
-    # append the basic information required in all behaviours to the dictionary
-    basicInfo = getBasicInfo(my_session)
-    
     # create the dictionary 'dictOut' which will be saved as a .mat file
     # this requires running the appropriate functions for the type of training
     # and merging the individual dictionaries using the line z = {**x, **y}
-
-    if sessionType == 'habituation':
-        dictOut = basicInfo
-    elif sessionType == 'recognition':
+   
+    if sessionType == 'imaging_stimulation':
+        imInfo = getImagingInfo(my_session)
+        dictOut = imInfo
+        
+    
+    elif sessionType == 'imaging_discrimination':
         training = getTraining(my_session)
-        dictOut = {**basicInfo, **training}
-    elif sessionType == 'discrimination':
-        training = getTraining(my_session)
-        discrim = getDiscrimination(my_session, go_pos)
-        dictOut = {**basicInfo, **training, **discrim}
+        imInfo = getImagingInfo(my_session)
+        dictOut = {**training, **imInfo}
 
+    else:
+        # get basic information required in all behaviours in the BSB 
+        basicInfo = getBasicInfo(my_session)
+        
+        if sessionType == 'habituation':
+            dictOut = basicInfo
+        elif sessionType == 'recognition':
+            training = getTraining(my_session)
+            dictOut = {**basicInfo, **training}
+        elif sessionType == 'discrimination':
+            training = getTraining(my_session)
+            discrim = getDiscrimination(my_session, go_pos)
+            dictOut = {**basicInfo, **training, **discrim}
+
+        
+        
+      
     saveMatStruct(dictOut, outPath, ID, sessionType, date)
         
 def getTxtFiles(fPath):
@@ -68,10 +83,21 @@ def getMetaData(my_session):
     
     go_pos = 'None'
     
+    name = my_session.experiment_name
+    
     # find the task the mouse did corresponding to the text file.
     # call the function relevant to that task 
-    if 'habituation' in my_session.experiment_name:      
+    if 'habituation' in name:      
         sessionType = 'habituation' 
+    
+    elif '2p' in name:
+        if 'whiskerstim' in name:
+            sessionType = 'imaging_stimulation'
+        elif 'trained_water' in name:
+            sessionType = 'imaging_discrimination'
+            go_pos = getGoPos(my_session)
+        else:
+            raise ValueError('unknown imaging behaviour')
         
         
     elif 'training' in my_session.experiment_name:
@@ -81,7 +107,7 @@ def getMetaData(my_session):
         else:     
             sessionType = 'recognition'
     else:
-        raise ValueError('Could not assign task to text file %s' %my_session.experiment_name)
+        raise ValueError('Could not assign task to text file %s' % my_session.experiment_name)
 
         
         
@@ -110,7 +136,7 @@ def getGoPos(my_session):
     if foundGo == False:
         print('%s' %my_session.subject_ID)
         print('%s' %my_session.datetime_string)
-        raise ValueError ('failed to find go position  in file %s' %my_session.subject_ID)
+        raise ValueError ('failed to find GO position in file %s' % my_session.subject_ID)
        
     return go_pos
     
@@ -119,30 +145,25 @@ def getGoPos(my_session):
 def searchPrintLines(my_session, I):
     
     '''
-    function that searches through the lines for the string 'I'. 
+    function that searches through the lines to see if the first word
+    after the time is the variable 'I'. 
     returns a list of the times (ms) that the string was printed
     during behaviour  
     '''
     
-    l = []
-    for line in my_session.print_lines:
-        if line.split()[1] == I:
-            value = line.split()[0]
-            l.append(value)
-    return l
+    l = [line.split()[0] for line in my_session.print_lines if line.split()[1] == I]
     
+    return l
         
     
 def getBasicInfo(my_session):
     
     basicInfo = {}
     
-    water = searchPrintLines(my_session, 'waterON')
-    
+    basicInfo['water_delivered'] = searchPrintLines(my_session, 'waterON')   
     basicInfo['running_forward'] =  my_session.times['going_forward']
     basicInfo['licks'] =  my_session.times['lick_event']    
-    basicInfo['water_delivered'] = water
-    
+   
     return basicInfo
 
 
@@ -152,13 +173,9 @@ def getTraining(my_session):
         
     training = {}
 
-    correct = searchPrintLines(my_session, 'correct')
-    missed = searchPrintLines(my_session, 'missed')
-    false =  searchPrintLines(my_session, 'false_positive')
-    
-    training['correct_trials'] =  correct
-    training['missed_trials'] = missed
-    training['falsepositive_trials'] = false
+    training['correct_trials'] = searchPrintLines(my_session, 'correct')
+    training['missed_trials']  = searchPrintLines(my_session, 'missed')
+    training['falsepositive_trials'] =  searchPrintLines(my_session, 'false_positive')
     
     return training
     
@@ -167,24 +184,47 @@ def getDiscrimination(my_session, go_pos):
     
     discrim = {}
         
-    reject = searchPrintLines(my_session,'rejection')
-    fp = searchPrintLines(my_session, 'false_positive')
-    lick = searchPrintLines(my_session, 'lick')
+    discrim['correctrejection_trials'] = searchPrintLines(my_session,'rejection')
+    discrim['falsepositive_trials'] = searchPrintLines(my_session, 'false_positive')
+    discrim['licks']  = searchPrintLines(my_session, 'lick')
 
     if go_pos == 1:
-        go =  searchPrintLines(my_session, 'forward_position')
-        nogo =  searchPrintLines(my_session, 'backward_position')
+        discrim['go_position']  =  searchPrintLines(my_session, 'forward_position')
+        discrim['nogo_position'] =  searchPrintLines(my_session, 'backward_position')
     elif go_pos == 0:
-        go = searchPrintLines(my_session, 'backward_position')
-        nogo = searchPrintLines(my_session, 'forward_position')
-   
-    discrim['correctrejection_trials'] = reject
-    discrim['falsepositive_trials'] = fp
-    discrim['licks'] = lick
-    discrim['go_position'] = go
-    discrim['nogo_position'] = nogo
-    
+        discrim['go_position'] = searchPrintLines(my_session, 'backward_position')
+        discrim['nogo_position'] = searchPrintLines(my_session, 'forward_position')
+  
     return discrim
+
+
+def getImagingInfo(my_session):
+    
+    '''
+    the behaviour under 2p is stored differently to in the bsb,
+    thus a new function is required    
+    '''
+    
+    imInfo = {}
+    imInfo['running_forward'] =  my_session.times['running mouse']
+    imInfo['running_backward'] = my_session.times['moonwalking mouse']
+    
+    #couldnt use 'searchPrintLines' for this because of the conflicting for 'going'
+    imInfo['motor_start'] = [line.split()[0] for line in my_session.print_lines if 'going forward' in line]
+     
+    return imInfo
+    
+    
+    
+
+
+
+
+
+
+
+
+
     
 
 def saveMatStruct(dictOut, outPath, ID, sessionType, date):
@@ -205,13 +245,16 @@ def saveMatStruct(dictOut, outPath, ID, sessionType, date):
 
 
 txtFiles = getTxtFiles(fPath)
-print(len(txtFiles))
+
 for txtFile in txtFiles:
-  
     try:
-        runWorkflow(txtFile, outPath)
-    except:
-        print(txtFile)
+        my_session = pc.Session(txtFile, False)
+    except StopIteration:
+        print('blank txt files in directory')
+        continue
+    
+        #runWorkflow(txtFile, outPath)
+    
 
         
     
