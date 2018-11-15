4e16c72f3191a2a07c26edf8a1f889f577bb1eaf
James Rowland <james.rowland@ccc.ox.ac.uk>
2018-11-15 13:30:10 +0000

    fixed bug in correctTooSoon

--- utils/correctTooSoon.m
+++ utils/correctTooSoon.m
@@ -1,24 +1,28 @@
-function corrected = correctTooSoon(correct, missed, correct_rejection, false_positive, too_soon)
-
-trial_comb = [correct,missed, correct_rejection,false_positive];
-
-t_window = 3000;
+function too_soon = correctTooSoon(correct, missed, correct_rejection, false_positive, too_soon)
 
 %if any too_soons occur within the time_window to another trial outcome,
 %remove it
-corrected = too_soon;
+trial_comb = [correct,missed, correct_rejection,false_positive];
+
+%time window encompassing trial (ms)
+t_window = 3000;
+
+ % index of too soons to delete 
+del_ind = [];
 
 for i = 1:length(too_soon)
     ts = too_soon(i);
     
     for ii = 1:length(trial_comb)
-        tc = trial_comb(ii);     
-    
-        if abs(ts-tc) < t_window          
-            corrected(i) = [];           
+        tc = trial_comb(ii);
+        
+        if abs(ts-tc) < t_window           
+            del_ind(end+1) = i;
         end
     end
 end
+
+too_soon(del_ind) = [];
 
 
 
